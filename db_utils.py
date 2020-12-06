bf527a560fd9030b0d5e060ed191f77d553585b8
Amogh J <amoghjahagirdargsf@gmail.com>
2020-12-06 10:37:21 +0400

    half-written create_habit fn

--- db_utils.py
+++ db_utils.py
@@ -8,11 +8,22 @@ firebase_admin.initialize_app(cd)
 datab = firestore.client()
 # usersref = datab.collection(u'staging')
 team_ref = datab.collection(u'staging').document(u'team')
-user_ref = team_ref.collection('users').document('user')
+user_ref = team_ref.collection('user').document('habits')
 user_ref.set({
     u'abs': ['Abhijeet','Sneha','Suresh'],
     u'habits': ['Bathe','Sleep'],
 })
+# A new user is created when he makes his first habit
+def create_habit(datab, env, team, user, habit_text, reminder_time, abs):
+    team_ref = datab.collection(env).document(team)
+    habit_ref = team_ref.collection(user).document('habits')
+    ab_ref = team_ref.collection(user).document('abs')
+    habit_info = habit_ref.get().to_dict()
+    ab_info = ab_ref.get().to_dict()
+    if habit_text not in habit_info:
+        habit_ref.set({
+            habit
+        })
 # docs = usersref.stream()
 
 # for doc in docs:
