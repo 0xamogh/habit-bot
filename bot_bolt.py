dd37053ef652294eb4243cf4ed869535e3ad07eb
Amogh J <amoghjahagirdargsf@gmail.com>
2021-02-01 21:31:09 +0400

    apphome listener

--- bot_bolt.py
+++ bot_bolt.py
@@ -81,7 +81,7 @@ habit_status_dict = {
     }
 gif_link = " "
 
-@app.message("hi")
+@app.message.app_home("hi")
 def reply(message, say):
     say("message recieve bruvv", channel = message['user'])
 
