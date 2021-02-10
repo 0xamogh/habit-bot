fa77684a6f7edcde45aeff9ecf0b57c2cca15d5c
Amogh J <amoghjahagirdargsf@gmail.com>
2021-02-10 10:29:13 +0400

    Plis work

--- bot_bolt.py
+++ bot_bolt.py
@@ -124,6 +124,7 @@ def submit_modal(ack, body, client, view):
 def open_home_tab(client, event = None, logger = None, user = None):
     build_home_tab_payload(client, db, gif_link, event, logger=None, user=None)
     check_stat = read_habit(db, event['user'])
+    print(check_stat)
     if check_stat.user_not_found:
         client.chat_postMessage(
             channel = event['user'],
